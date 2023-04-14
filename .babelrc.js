440cb3a3cc20f7ca9b6e3fe2a515c4f8f205a6bb
Trim21 <trim21.me@gmail.com>
2023-04-15 03:50:53 +0800

    remove babelrc

--- .babelrc.js
+++
@@ -1,6 +0,0 @@
-// testing env babel config is located at ./src/test/babel-register.js
-
-// do not add presets-env here, building output target is configured in rollup config
-module.exports = {
-  presets: [['@babel/preset-typescript']],
-}
